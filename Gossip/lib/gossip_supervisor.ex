8e26f87b1bd8eebd0915a6462f0dfa45c66ccb18
Nishant Agarwal <nishant.fiem@gmail.com>
2017-10-07 14:41:56 -0400

    final changes

--- Gossip/lib/gossip_supervisor.ex
+++ Gossip/lib/gossip_supervisor.ex
@@ -1,20 +1,20 @@
 defmodule GossipChildModule do
     use Supervisor
 
-    def start_link(numNodes, topology, algorithm) do
-        Supervisor.start_link(__MODULE__, [numNodes, topology, algorithm])
+    def start_link(numNodes, topology, algorithm, mainProcessID) do
+        Supervisor.start_link(__MODULE__, [numNodes, topology, algorithm, mainProcessID], name: __MODULE__)
     end
 
-    def init([numNodes, topology, algorithm]) do
+    def init([numNodes, topology, algorithm, mainProcessID]) do
         IO.puts("Gossip Supervisor Started")        
-        children = create_gossip_children(numNodes, topology, algorithm)
-        children = [worker(PushSumWorkers, [numNodes]) | children]
+        children = create_gossip_children(numNodes, topology, algorithm, mainProcessID)
+        children = [worker(PushSumWorkers, [numNodes, mainProcessID]) | children]
         IO.puts "All actors created"          
         supervise(children, strategy: :one_for_one)          
     end
 
-    def create_gossip_children(totalNumberNodes,topology, algorithm) do        
-        childrenWorkerList = Enum.reduce(1..totalNumberNodes, [], fn(x, acc) -> [worker(WorkerModule, [x, totalNumberNodes, topology, algorithm], [id: x, restart: :temporary]) | acc] end)
+    def create_gossip_children(totalNumberNodes,topology, algorithm, mainProcessID) do        
+        childrenWorkerList = Enum.reduce(1..totalNumberNodes, [], fn(x, acc) -> [worker(WorkerModule, [x, totalNumberNodes, topology, algorithm, mainProcessID], [id: x, restart: :temporary]) | acc] end)
         childrenWorkerList
     end   
 end
